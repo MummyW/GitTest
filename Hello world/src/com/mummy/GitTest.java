941ec5914c328cb8f5750754cf420838fcd3e2e2
ZF <1838947259@qq.com>
2017-11-25 11:44:10 +0800

    second

--- Hello world/src/com/mummy/GitTest.java
+++ Hello world/src/com/mummy/GitTest.java
@@ -1,8 +1,8 @@
-package com.mummy;
+﻿package com.mummy;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world 1");
 		System.out.println("n您好");
 	}
 }
